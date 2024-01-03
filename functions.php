f6b039e40b5281eabffb2c23d8c4ac96792eb331
Bobikus Manjarus <robertjaros235@gmail.com>
2024-01-03 18:31:50 +0100

    q fix

--- functions.php
+++ functions.php
@@ -19,9 +19,9 @@ function print_competitions(array $competition_list){
     for($i = 0; $i < count($competition_list); $i++){
         $competition = $competition_list[$i];
         echo "<article>";
-        echo "<h3><a href='competition?id=$competition[id]'> $competition[title] </a></h3>";
+        echo "<h3><a href='competition?id=$competition[id]'> ".htmlspecialchars($competition["title"])." </a></h3>";
         echo "<div class='date_event'>".date_format(date_create($competition["date_event"]), "j. n. Y G:i")."</div>";
-        echo "<div>$competition[description]</div>";
+        echo "<div>".htmlspecialchars($competition["description"])."</div>";
         echo "</article>";
         if(strtotime($competition["date_event"]) > time() && isset($competition_list[$i+1])){
             if(strtotime($competition_list[$i+1]["date_event"]) < time()){
